ddb192c72697e6f3c009b3695338c24f72e92a5c
mmzhw <mmzhw51@gmail.com>
2025-01-10 14:43:26 +0800

    更新流程

--- src/request/mainRequset4.js
+++ src/request/mainRequset4.js
@@ -9,10 +9,28 @@ const appName = 'www.52damu.com';
 
 async function myTask() {
     try {
-        let formData = new FormData();
-        formData.append('nonce', '0885a8c731');
-        formData.append('action', 'zb_user_qiandao');
         let authorization = fs.readFileSync(path.resolve('./cookies4'), 'utf8').toString().trim();
+        let response = await axios.get('https://www.52damu.com/user/', {
+            headers: {
+                'Cookie': authorization,
+            },
+        })
+        const regex = /"ajax_nonce":"(.*?)"/;
+        const match = response.data.match(regex);
+
+        let nonce = ''
+
+        if (match && match[1]) {
+            nonce = match[1];
+            console.log(moment().format('YYYY-MM-DD HH:mm:ss'), appName, '找到 ajax_nonce 的值：', nonce);
+        } else {
+            console.log(moment().format('YYYY-MM-DD HH:mm:ss'), appName, '未找到 ajax_nonce 的值');
+            return
+        }
+
+        let formData = new FormData();
+        formData.append('nonce', nonce);
+        formData.append('action', 'zb_user_qiandao');
 
         let result = await axios.post('https://www.52damu.com/wp-admin/admin-ajax.php', formData, {
             headers: {
@@ -30,5 +48,7 @@ async function mainRequest() {
     scheduleDailyTask(myTask, appName, false);
 }
 
-module.exports = {mainRequest,myTask}
+module.exports = {mainRequest, myTask}
+
+
 // myTask()
