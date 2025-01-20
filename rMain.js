22778e0f2189f9c1453bbf967c8d4a2e1d710595
mmzhw <mmzhw51@gmail.com>
2025-01-20 11:53:33 +0800

    去除失效的

--- rMain.js
+++ rMain.js
@@ -1,5 +1,5 @@
 // require('./src/request/mainRequset1').mainRequest();
-require('./src/request/mainRequset2').mainRequest();
+// require('./src/request/mainRequset2').mainRequest();
 // require('./src/request/mainRequset3').mainRequest();
 require('./src/request/mainRequset4').mainRequest();
 require('./src/request/mainRequset5').mainRequest();
