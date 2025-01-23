33f6361dac9b3f7600c35370e6b7eac44b4f73f8
mmzhw <mmzhw51@gmail.com>
2025-01-23 19:47:32 +0800

    更新

--- src/request/mainRequset5.js
+++ src/request/mainRequset5.js
@@ -9,14 +9,14 @@ const appName = 'www.caijihao.com';
 
 async function myTask() {
     try {
-        let formData = new FormData();
-        formData.append('nonce', 'ffe839b950');
-        formData.append('action', 'zb_user_qiandao');
+        // let formData = new FormData();
+        // formData.append('nonce', 'ffe839b950');
+        // formData.append('action', 'zb_user_qiandao');
         let authorization = fs.readFileSync(path.resolve(__dirname, './cookies5'), 'utf8').toString().trim();
 
-        let result = await axios.post('https://www.caijihao.com/wp-json/b2/v1/userMission', formData, {
+        let result = await axios.post('https://www.caijihao.com/wp-json/b2/v1/userMission', {}, {
             headers: {
-                'Content-Type': 'multipart/form-data',
+                'Content-Type': 'application/json; charset=UTF-8',
                 'Cookie': authorization,
                 'authorization': 'Bearer ' + authorization,
             },
